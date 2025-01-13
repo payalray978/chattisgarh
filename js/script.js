d301ddb396753daf425844657824d83cab7813c1
payal <payal@csm.tech>
2025-01-13 13:58:39 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -57,5 +57,10 @@ document.getElementById("selectIndustryType").addEventListener("change", functio
 // });
 
 
-
-    
+//tooltip
+    document.addEventListener('DOMContentLoaded', function () {
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+            tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+                new bootstrap.Tooltip(tooltipTriggerEl);
+            });
+        });
